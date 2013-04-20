f3600848030a84ddb8d5431f2297bc1395bda150
Julian Day <jcd748@mail.usask.ca>
2013-04-20 11:44:57 -0500

    Problem 22 - names scoring, and the associated data file.

---
+++ problem22.scm
@@ -0,0 +1,46 @@
+;;; Project Euler, problem 22 - Name scores
+;;; Copyright 2013 Julian Day <jcd748@mail.usask.ca>
+
+;; word-score assumes that the "word" is passed in as a list of letters.
+;; The word score return value is determined by summing the alphabetical
+;; position of each letter.
+(define word-score
+  (lambda (word)
+    (if (null? word)
+        0
+        (+ (+ 1 (char- (car word) #\A))
+           (word-score (cdr word))))))
+
+;; Read a CSV file into a list.  Assumption is that everything between
+;; commas is quoted.
+(define csvfile->list
+  (lambda (filename)
+    (let ((lst '())
+          (file (open-input-file filename)))
+      (let loop ((cur-val (read file))
+                 (llst lst))
+        (if (eof-object? cur-val)
+            llst
+            ; Check to see if the first character is a #\,
+            ; Everything after the first element should be.
+            (if (pair? cur-val)
+                (loop (read file) (append llst (cdr cur-val)))
+                ; If it's not a pair, it'll be the first item
+                ; read, which will be a string.
+                (loop (read file) (cons cur-val '()))))))))
+
+;; The list-score is determined by multiplying the word-score of each item in
+;; the list by its index within the list, and summing the result.  It is
+;; assumed that each item in the list is represented as a string.
+(define list-score
+  (lambda (lst idx)
+    (if (null? lst)
+        0
+        (+ (* (word-score (string->list (car lst))) idx)
+           (list-score (cdr lst) (+ idx 1))))))
+
+;; Read in the list of names into a list of strings, sort them alphabetically,
+;; and then get the list score.
+(define problem-22
+  (lambda (fname)
+    (list-score (sort string<? (csvfile->list fname)) 1)))
