ae058be3a376d0b118e46736669dacf9eb66e52b
Julian Day <jcd748@mail.usask.ca>
2013-06-30 09:07:11 -0500

    Problem 11 - largest adjacent product in grid.

---
+++ problem11.scm
@@ -0,0 +1,132 @@
+;;; Project Euler, problem 11.
+;;;
+;;; Copyright (c) 2013 Julian Day <jcd748@mail.usask.ca>
+
+;; Define the multi-dimensional array, which we'll treat as a flat vector
+;; for speed purposes.
+(define grid '#(
+08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48))
+
+;; Keep track that this is a two dimensional array, and that each array
+;; has 20 values
+(define grid-size 20)
+(define last-grid-index (- grid-size 1))
+
+;; Get the single-dimensional index given a conceptual row and column
+;; for a 2d array.
+(define grid-index
+  (lambda (row col)
+    (+ (* row grid-size) col)))
+
+;; Get a particular item in the grid.
+(define grid-item
+  (lambda (row col)
+    (vector-ref grid (grid-index row col))))
+
+;; Check to see if we can find a certain number of adjacent items in a
+;; given direction (if we're at the second-last element in a row, then there
+;; isn't any way to get four adjacent horizontally (to the right) from there.
+(define can-find-adjacent?
+  (lambda (num-adj row col direction)
+    (cond ((eq? direction 'horizontal) (>= last-grid-index (+ col num-adj)))
+          ((eq? direction 'vertical) (>= last-grid-index (+ row num-adj)))
+          ((eq? direction 'diag-up) (and (>= (- row num-adj) 0)
+                                         (>= last-grid-index (+ col num-adj))))
+          ((eq? direction 'diag-down) (and (>= last-grid-index (+ row num-adj))
+                                           (>= last-grid-index (+ col num-adj))))
+          (else #f))))
+
+;; Get the product function for the given direction
+(define get-prod-fn
+  (lambda (num-adj row col direction)
+    (if (can-find-adjacent? num-adj row col direction)
+        prod-fn
+        null-fn)))
+
+(define get-row-fn
+  (lambda (direction)
+    (cond ((eq? direction 'horizontal) id)
+          ((eq? direction 'vertical) incr)
+          ((eq? direction 'diag-up) decr)
+          ((eq? direction 'diag-down) incr)
+          (else id))))
+
+(define get-col-fn
+  (lambda (direction)
+    (cond ((eq? direction 'horizontal) incr)
+          ((eq? direction 'vertical) id)
+          ((eq? direction 'diag-up) incr)
+          ((eq? direction 'diag-down) incr)
+          (else id))))
+
+;; The product function if a bad direction is provided, or we can't multiply
+;; adjacent values
+(define null-fn
+  (lambda (num-adj row col next-row-fn next-col-fn)
+    0))
+
+(define incr
+  (lambda (n)
+    (+ n 1)))
+
+(define decr
+  (lambda (n)
+    (- n 1)))
+
+(define id
+  (lambda (n) 
+    n))
+
+(define prod-fn
+  (lambda (num-adj row col next-row-fn next-col-fn)
+    (let loop ((cur-row row)
+               (cur-col col)
+               (product 1)
+               (adj-rem (- num-adj 1)))
+      (if (< adj-rem 0)
+          product
+          (loop (next-row-fn cur-row)
+                (next-col-fn cur-col)
+                (* product (grid-item cur-row cur-col))
+                (- adj-rem 1))))))
+
+(define adjacent-prod
+  (lambda (num-adj row col direction)
+    (let ((prod-fn (get-prod-fn num-adj row col direction)))
+      (prod-fn num-adj row col (get-row-fn direction) (get-col-fn direction)))))
+
+(define problem-11
+  (lambda (num-adj)
+    (let loop ((row 0)
+               (col 0)
+               (best 0))
+      (if (and (= row last-grid-index)
+               (= col last-grid-index))
+          best ; we've reached the end - return the greatest value
+          (loop (if (= col last-grid-index) (incr row) row)
+                (if (= col last-grid-index) 0 (incr col))
+                (max best (max (adjacent-prod num-adj row col 'horizontal)
+                               (adjacent-prod num-adj row col 'vertical)
+                               (adjacent-prod num-adj row col 'diag-up)
+                               (adjacent-prod num-adj row col 'diag-down))))))))
+          
+               
